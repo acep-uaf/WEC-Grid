2bdf22dbdc5ce1b9aa366905a2811d5733f047f1
Alex Barajas <barajale@oregonstate.edu>
2025-08-26 02:37:46 -0700

    error in docs - wec_farm.md

---
+++ src/wecgrid/modelers/wec_sim/__init_temp__.py
@@ -0,0 +1,9 @@
+"""
+WEC-Sim integration module for WEC-Grid.
+
+Provides interfaces for running WEC-Sim device-level simulations.
+"""
+
+from .runner import WECSimRunner
+
+__all__ = ["WECSimRunner"]
